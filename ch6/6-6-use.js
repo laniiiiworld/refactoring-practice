cceb9f6264844ba986237ec85193b5534d66fa0d
lani <laniiiiworld@gmail.com>
2023-08-22 18:42:53 +0900

    6-6. 변수 캡슐화하기

--- ch6/6-6-use.js
+++ ch6/6-6-use.js
@@ -1,6 +1,7 @@
+//변수 캡슐화하기
 import { getDefaultOwner } from './6-6.js';
 
 const owner = getDefaultOwner();
-owner.firstName = '엘리';
-console.log(owner);
+// owner.firstName = '엘리';
+console.log(owner.firstName);
 console.log(getDefaultOwner());
