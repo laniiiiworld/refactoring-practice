35ad75cb84a798d53ae56c9e40025fac4dcc8f61
lani <laniiiiworld@gmail.com>
2023-08-30 20:43:27 +0900

    7-3. 기본형을 객체로 바꾸기

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -1,15 +1,41 @@
-export class Order {
-  constructor(data) {
-    this.priority = data.priority;
+//기본형을 객체로 바꾸기
+{
+  class Order {
+    constructor(priority) {
+      this.priority = priority;
+    }
+    isHighPriority() {
+      return this.priority.higherThan(new Priority('normal'));
+    }
   }
+
+  class Priority {
+    #value;
+    constructor(value) {
+      if (!Priority.legalValues().includes(value)) {
+        throw new Error(`${value} is invalid for Priority`);
+      }
+      this.#value = value;
+    }
+    get index() {
+      return Priority.legalValues().indexOf(this.#value);
+    }
+    equals(other) {
+      return this.index === other.index;
+    }
+    higherThan(other) {
+      return this.index > other.index;
+    }
+    static legalValues() {
+      return ['low', 'normal', 'high', 'rush'];
+    }
+  }
+
+  const orders = [
+    new Order(new Priority('normal')), //
+    new Order(new Priority('high')),
+    new Order(new Priority('rush')),
+  ];
+  const highPriorityCount = orders.filter((o) => o.isHighPriority()).length;
+  console.log(highPriorityCount);
 }
-
-const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
-];
-
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
