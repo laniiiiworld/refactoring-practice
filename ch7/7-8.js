cb6f93768babb054bf5d708658ddacdda5bc53e2
lani <laniiiiworld@gmail.com>
2023-08-31 22:16:08 +0900

    7-8. 중개자 제거하기

---
+++ ch7/7-8.js
@@ -0,0 +1,37 @@
+//중개자 제거하기
+//중개자 = 위임 = 컴포지션
+class Person {
+  #name;
+  #manager;
+  #chargeCode;
+  constructor(name, manager, chargeCode) {
+    this.#name = name;
+    this.#manager = manager;
+    this.#chargeCode = chargeCode;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get manager() {
+    return this.#manager;
+  }
+
+  set manager(arg) {
+    this.#manager = arg;
+  }
+
+  get chargeCode() {
+    return this.#chargeCode;
+  }
+
+  set chargeCode(arg) {
+    this.#chargeCode = arg;
+  }
+}
+
+const person = new Person('Tom', 'aManager', '999');
+console.log(person.name);
+console.log(person.manager);
+console.log(person.chargeCode);
