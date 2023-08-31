59bb46d7ad18d4bd622767cae5f75c592b844b36
lani <laniiiiworld@gmail.com>
2023-08-31 21:49:05 +0900

    7-5. 클래스 추출하기

--- ch7/7-5.js
+++ ch7/7-5.js
@@ -1,39 +1,49 @@
+//클래스 추출하기
 class Person {
   #name;
-  #officeAreaCode;
-  #officeNumber;
+  #telephoneNumber;
   constructor(name, areaCode, number) {
     this.#name = name;
-    this.#officeAreaCode = areaCode;
-    this.#officeNumber = number;
+    this.#telephoneNumber = new TelephoneNumber(areaCode, number);
   }
-
   get name() {
     return this.#name;
   }
-
   set name(arg) {
     this.#name = arg;
   }
-
   get telephoneNumber() {
-    return `(${this.officeAreaCode}) ${this.officeNumber}`;
+    return this.#telephoneNumber.toString;
   }
-
   get officeAreaCode() {
-    return this.#officeAreaCode;
+    return this.#telephoneNumber.areaCode;
   }
-
-  set officeAreaCode(arg) {
-    this.#officeAreaCode = arg;
-  }
-
   get officeNumber() {
-    return this.#officeNumber;
+    return this.#telephoneNumber.number;
   }
+}
 
-  set officeNumber(arg) {
-    this.#officeNumber = arg;
+class TelephoneNumber {
+  #areaCode;
+  #number;
+  constructor(areaCode, number) {
+    this.#areaCode = areaCode;
+    this.#number = number;
+  }
+  get areaCode() {
+    return this.#areaCode;
+  }
+  set areaCode(arg) {
+    this.#areaCode = arg;
+  }
+  get number() {
+    return this.#number;
+  }
+  set number(arg) {
+    this.#number = arg;
+  }
+  get toString() {
+    return `(${this.#areaCode}) ${this.#number}`;
   }
 }
 
